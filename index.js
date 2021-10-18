a338ea5e999b1bd13e20148ffb0628177f946946
Ronny Leibham <ronnyleibham@gmx.de>
2021-10-18 12:10:42 +0200

    Add nodemon

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { getGreeting } from './getGreeting.js'
 import { replaceName } from './replaceName.js'
 
-console.log(getGreeting('Poperze'))
+console.log(getGreeting('PoperzenPeter'))
 
 console.log(replaceName('Hello Jane', 'Jane', 'John'))
 console.log(replaceName('Hello Jane. Goodbye Jane.', 'Jane', 'John'))
