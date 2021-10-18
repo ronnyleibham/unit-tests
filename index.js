c28dcd29a33bb4a233317918fceb70a9a6e3f0bc
Ronny Leibham <ronnyleibham@gmx.de>
2021-10-18 11:41:56 +0200

    Add replaceName

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function getGreeting(name = 'you') {
 }
 
 console.log(getGreeting('Poperze'))
+
+function replaceName(text, name, newName) {
+  return text.replaceAll(name, newName)
+}
+
+console.log(replaceName('Hello Jane', 'Jane', 'John'))
+console.log(replaceName('Hello Jane. Goodbye Jane.', 'Jane', 'John'))
